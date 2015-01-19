7453cabc6f3863e929998540cf276b00fe88892b
Braman2@csc.com <braman2@CSCINDAH502520.asiapac.globalcsc.net>
2015-01-19 10:44:41 +0530

    Updated Source Code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix - Create an augmented matrix object
+##         that contains functions to store the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  ##Initialize the inverse  
+  i <- NULL
+  ##set function to intialize augmented matrix object  
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
 
+  ##get function to retrieve augment matrix object
+  get <- function() x
+
+  ##setinverse function to set inverse of the matrix  
+  setinverse <- function(inverse) i <<- inverse
+
+  ##setinverse function to get the inverse of the matrix
+  getinverse <- function() i
+
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+##  computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve 
+## the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+
+  ##if inverse exists and matrix has not changed
+  ## If matrix has not changed, matrix product should give identity matrix
+
+  data <- x$get()
+  i <- x$getinverse()
+  if (!is.null(i)  && !identical(data,i)) {
+    message("getting cached data")
+    return(i)
+
+  }
+
+  ## Compute inverse, set the cache and return the inverse
+  i <- solve(data)
+  x$setinverse(i)
+  return(i)
 }
+
+x <- matrix(c(1, 2, 3, 4), nrow = 2, ncol = 2, byrow = TRUE)
+m <- makeCacheMatrix(x)
+
+# original matrix
+m$get()
+
+# first call is uncached
+cacheSolve(m)
+
+# second call is cached
+cacheSolve(m)
